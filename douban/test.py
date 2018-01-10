a3607c2f5c65ec7f2748327ebfdedbe290ac2f2b
Sucreyy1 <593143487@qq.com>
2018-01-10 17:49:56 +0800

    豆瓣模拟登陆..慎用,会造成暂时的ip封禁

---
+++ douban/test.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+data = {
+    'source': 'index_anv',
+    'redir': 'https://www.douban.com/',
+    'login': '登陆'
+}
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 '
+                  'Safari/537.36 '
+}
+urls = 'https://www.douban.com/login'
+after_url = 'https://www.douban.com/people/95231173/'
+
+
+def login(user, passwd, url):
+    data['form_email'] = user
+    data['form_password'] = passwd
+    s = requests.session()
+    login_result = s.post(url, data=data, headers=header)
+    print(login_result.status_code)
+    code = get_captcha(login_result.text)
+    data['captcha-id'] = code
+    login_result = s.post(url, data=data, headers=header)
+    print(login_result.text)
+    response = s.get(after_url, cookies=login_result.cookies, headers=header)
+    return response.content
+
+
+def get_captcha(url_cont):
+    soup = BeautifulSoup(url_cont, 'html.parser')
+    captcha = soup.find('div', class_='captcha_block').find('input', type='hidden')
+    print(captcha.get('value'))
+    code = captcha.get('value')
+    return code
+
+
+if __name__ == '__main__':
+    username = ''
+    password = ''
+    cont = login(username, password, urls)
+    # print(cont.decode('utf-8'))
