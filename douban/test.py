a5d12f367d4bdd4b368be82ce116b8e86dc0c63b
Sucreyy1 <593143487@qq.com>
2018-01-10 21:45:54 +0800

    豆瓣模拟登陆...成型版

--- douban/test.py
+++ douban/test.py
@@ -19,10 +19,13 @@ def login(user, passwd, url):
     data['form_password'] = passwd
     s = requests.session()
     login_result = s.post(url, data=data, headers=header)
-    print(login_result.status_code)
+    print(login_result.text)
+    # 验证是否需要验证码
     code = get_captcha(login_result.text)
-    data['captcha-id'] = code
-    login_result = s.post(url, data=data, headers=header)
+    if code is None:
+        # 如果需要验证码则获取验证码
+        data['captcha-id'] = code
+        login_result = s.post(url, data=data, headers=header)
     print(login_result.text)
     response = s.get(after_url, cookies=login_result.cookies, headers=header)
     return response.content
@@ -30,7 +33,10 @@ def login(user, passwd, url):
 
 def get_captcha(url_cont):
     soup = BeautifulSoup(url_cont, 'html.parser')
-    captcha = soup.find('div', class_='captcha_block').find('input', type='hidden')
+    captcha = soup.find('div', class_='captcha_block')
+    if captcha is None:
+        return None
+    captcha = captcha.find('input', type='hidden')
     print(captcha.get('value'))
     code = captcha.get('value')
     return code
@@ -40,4 +46,4 @@ if __name__ == '__main__':
     username = ''
     password = ''
     cont = login(username, password, urls)
-    # print(cont.decode('utf-8'))
+    print(cont.decode('utf-8'))
